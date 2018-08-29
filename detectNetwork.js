730c71d3640dfbc541ded1420e956671ff5afdf0
PTSoup <phaimani.souphaphone@gmail.com>
2018-08-29 18:32:44 -0500

    Update function to detect China UnionPay and Switch/refactor code
    Updated the function to include China Unionpay and Switch network detection. Refactored code to bring processing time down.

--- detectNetwork.js
+++ detectNetwork.js
@@ -8,25 +8,61 @@
 //   2. The number of digits in the number (called the length)
 
 var detectNetwork = function(cardNumber) {
+
+    // helper functions
     var nums = cardNumber.length;
-    var key = Number(cardNumber.slice(0, 2));
-
-    if ([13, 16, 19].includes(nums) && Number(cardNumber.charAt(0)) === 4) {
-        return `Visa`;
+    var range = function(start, end) {
+        var range = [];
+        for (var i = start; i <= end; i++) {
+            range.push(i);
+        }
+        return range;
     };
 
-    if (nums === 14 && [38, 39].includes(key)) {
-        return `Diner's Club`;
-    } else if ([34, 37].includes(key) && nums === 15) {
-        return `American Express`;
-    } else if ([51, 52, 53, 54, 55].includes(key) && nums === 16) {
-        return `MasterCard`;
-    } else if ([60, 64, 65].includes(key) && (nums === 16 || nums === 19)) {
-        return `Discover`;
-    } else if ([50, 63].includes(key) && (nums <= 12 || nums <= 19)) {
-        return `Maestro`;
-    } else {
-        return `Network Not Found`;
+    var american = [34, 37];
+    var mastercard = range(51,55);
+    var switc = [4903, 4905, 4911, 4936, 564182, 633110, 6333, 6759];
+    var visa = [4];
+    var diners = [38, 39];
+    var discover = range(644, 649).concat([6011, 65]);
+    var maestro = [5018, 5020, 5038, 6304];
+    var china = range(624, 626).concat(range(6282, 6288)).concat(range(622126, 622925));
+
+    var cardCheck = function(key) {
+
+        
+        if (switc.includes(key) && [16, 18, 19].includes(nums)) {
+            return `Switch`;
+        } else if (Number(key) === 4 && [13, 16, 19].includes(nums)) {
+            return `Visa`;
+        };
+
+
+        if (diners.includes(key) && nums === 14) {
+            return `Diner's Club`;
+        } else if (american.includes(key) && nums === 15) {
+            return `American Express`;
+        } else if (mastercard.includes(key) && nums === 16) {
+            return `MasterCard`;
+        } else if (discover.includes(key) && (nums === 16 || nums === 19)) {
+            return `Discover`;
+        } else if (maestro.includes(key) && (nums <= 12 || nums <= 19)) {
+            return `Maestro`;
+        } else if (china.includes(key) && (nums <= 16 || nums <= 19)) {
+            return 'China UnionPay';
+        }
+
+
     };
+
+    // check
+    var start = Number(cardNumber.slice(0, 2)) === 49 ? 2 : 1;
+
+    for (var i = start; i < nums; i++) {
+        var key = Number(cardNumber.slice(0, i));
+        if (cardCheck(key)) {
+            return cardCheck(key);
+        }
+    }
+    return `No network found`;
 };
-
